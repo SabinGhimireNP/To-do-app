484b8028bb6d404bbdc5b30a118060d783a44c86
Sabin Ghimire <sabinghimire223@gmail.com>
2025-01-07 09:03:55 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let var1 = document.querySelector(".input");
+let btn = document.querySelector(".add");
+let todo = document.querySelector(".list");
+// console.log(var1);
+// console.log(btn);
+// console.log(todo);
+/////to get the input/////
+btn.addEventListener("click", function () {
+  let inputValue = var1.value;
+  let li = document.createElement("li");
+  let deletebutton = document.createElement("button");
+  deletebutton.innerHTML = "Task Completed";
+  li.innerHTML = inputValue;
+  deletebutton.addEventListener("click", function () {
+    todo.removeChild(li);
+    todo.removeChild(deletebutton);
+  });
+
+  // console.log(deletebutton);
+  todo.appendChild(li);
+  todo.appendChild(deletebutton);
+});
