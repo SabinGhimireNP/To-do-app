e8cee8d25db207c7fc61154667a0278741b463da
Sabin Ghimire <sabinghimire223@gmail.com>
2025-06-30 10:48:06 +0545

    local storage

--- script.js
+++ script.js
@@ -1,24 +1,77 @@
+// let var1 = document.querySelector(".input");
+// let btn = document.querySelector(".add");
+// let todo = document.querySelector(".list");
+// let da = "";
+// btn.addEventListener("click", function () {
+//   let inputValue = var1.value;
+//   let li = document.createElement("li");
+//   let deletebutton = document.createElement("button");
+//   deletebutton.innerHTML = "Task Completed";
+//   li.innerHTML = inputValue;
+//   if (var1.value === da) {
+//     alert("Please Enter a valid input");
+//   } else {
+//     todo.appendChild(li);
+//     todo.appendChild(deletebutton);
+//     todo.appendChild(li);
+//     todo.appendChild(deletebutton);
+//   }
+
+//   deletebutton.addEventListener("click", function () {
+//     todo.removeChild(li);
+//     todo.removeChild(deletebutton);
+//   });
+// });
+
 let var1 = document.querySelector(".input");
 let btn = document.querySelector(".add");
 let todo = document.querySelector(".list");
-let da = "";
+
+// Load saved tasks on page load
+window.onload = function () {
+  loadData();
+};
+
 btn.addEventListener("click", function () {
-  let inputValue = var1.value;
+  let inputValue = var1.value.trim();
+  if (!inputValue) {
+    alert("Please enter a valid input");
+    return;
+  }
+
+  addTask(inputValue);
+  var1.value = "";
+  saveData();
+});
+
+function addTask(taskText) {
   let li = document.createElement("li");
   let deletebutton = document.createElement("button");
   deletebutton.innerHTML = "Task Completed";
-  li.innerHTML = inputValue;
-  if (var1.value === da) {
-    alert("Please Enter a valid input");
-  } else {
-    todo.appendChild(li);
-    todo.appendChild(deletebutton);
-    todo.appendChild(li);
-    todo.appendChild(deletebutton);
-  }
+  li.innerHTML = taskText;
+
+  todo.appendChild(li);
+  todo.appendChild(deletebutton);
 
   deletebutton.addEventListener("click", function () {
     todo.removeChild(li);
     todo.removeChild(deletebutton);
+    saveData();
   });
-});
+}
+
+function saveData() {
+  let tasks = [];
+  let items = todo.querySelectorAll("li");
+  items.forEach((li) => {
+    tasks.push(li.innerHTML);
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadData() {
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.forEach((task) => {
+    addTask(task);
+  });
+}
